7c3d561b9c3da7a39cd17b416cf186d1a96e4917
Festus Kiprono <festuskipchirchir1@gmail.com>
2026-07-08 09:46:57 +0300

    Initial set up from spring initializr, add docker compose, and gitignore

---
+++ src/main/java/com/skysurvey/sky_survey_api/SkySurveyApiApplication.java
@@ -0,0 +1,13 @@
+package com.skysurvey.sky_survey_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkySurveyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkySurveyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skysurvey/sky_survey_api/SkySurveyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skysurvey.sky_survey_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkySurveyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
